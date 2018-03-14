5c8c1691b45d0aae15b60778db64c6389906a847
curso-tajamar- <edissn4400@hotmail.com>
2018-03-14 20:15:15 +0100

    fff

---
+++ trabajo curso/JS/01 inicio/js/app.js
@@ -0,0 +1,5 @@
+import{saludar} from'./saludos.js'
+
+console.log('hola desde un fichero')
+
+saludar();
---
+++ trabajo curso/JS/02 funciones/js/app.js
@@ -0,0 +1,9 @@
+//Nombres en js utilizan camelcase
+
+function saludar(){
+    console.log('Hola amigo')
+    
+
+}
+
+//saludar()
