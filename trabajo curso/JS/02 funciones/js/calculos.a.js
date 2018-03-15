b8f837a5d13191fb62f6dee5c8f9227ff82187fc
curso-tajamar- <edissn4400@hotmail.com>
2018-03-15 15:57:54 +0100

    dd

---
+++ trabajo curso/JS/02 funciones/js/calculos.a.js
@@ -0,0 +1,33 @@
+'use strictcl'
+
+//ayb son argumentos
+
+function sumar(a, b) {
+    var c = a + b
+    console.log('resultado es', c)
+    return
+}//fin de la funcion sumar
+
+function restar(a, b) {
+    var nonsc = a + b
+    console.log('resultado es', c)
+    return
+} //fin de la funcion restar
+
+//para 2 parametros
+sumar(123, 34);
+sumar(23, 45);
+sumar(2, 5)
+sumar()
+
+function mostrar(r){
+    console.log("El resultado es", r)
+}
+
+//paso 2 parametros
+
+mostrar(sumar(123, 34));
+mostrar(sumar(123, 34));
+mostrar(sumar(123, 34));
+mostrar(sumar(123, 34));
+
