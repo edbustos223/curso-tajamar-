db534435aa070fdfb6ed603a880fd6cc8f972808
curso-tajamar- <edissn4400@hotmail.com>
2018-03-16 18:16:37 +0100

    salto de linea

--- trabajo curso/JS/03_sentencias/js/condiciones.js
+++ trabajo curso/JS/03_sentencias/js/condiciones.js
@@ -1,56 +1,71 @@
-const user = {
-    nombre : "pepe" ,
+const userpepe = {
+    nombre: "pepe",
     apellido: "perez",
-    edad : 17,
-        aficiones:[`deporte`, `papiroflexia`],
-        isCasado: false,
-        genero: `M`
- }
+    edad: 17,
+    aficiones: [`deporte`, `papiroflexia`],
+    isCasado: false,
+    genero: `M`
+}
 
 
 
- const user = {
-    nombre : "maria" ,
+const usermaria = {
+    nombre: "maria",
     apellido: "lopez",
-    edad : 53,
-        aficiones:[`papiroflexia`, `lectura`],
-        isCasado: true,
-        genero: `F`
- }
+    edad: 53,
+    aficiones: [`papiroflexia`, `lectura`],
+    isCasado: true,
+    genero: `F`
+}
 
 const MAYORIAEDAD = 18
- 
 
-function crearsaludo(user){
-    let mensaje  =`hola`
 
-    if(user.edad>= MAYORIAEDAD && user.genero == `M`){
-mensaje += `D.`
-    } else if(user.edad>= MAYORIAEDAD && user.genero == `f`){
+function crearsaludo(user) {
+    let mensaje = `hola`
+
+    if (user.edad >= MAYORIAEDAD && user.genero == `M`) {
+        mensaje += `D.`
+    } else if (user.edad >= MAYORIAEDAD && user.genero == `f`) {
         mensaje += `Dña`
-    
-
     }
 
-   mensaje + user.nombre
-if (user.isCasado) {
-    if(user.genero == `M`){
-   mensaje += `. saludos a tu mujer`
-} else{
-    mensaje +=`. saludos a tu marido`
-    } 
-}
-return mensje
-}
-
-function saludar(usermaria){
-    console.log(crearsaludo (usuario))
+    mensaje + user.nombre
+    if (user.isCasado) {
+        if (user.genero == `M`) {
+            mensaje += `. saludos a tu mujer`
+        } else {
+            mensaje += `. saludos a tu marido`
+        }
+    }
+    return mensaje
 }
 
+function saludar(usuario) {
+    console.log(crearsaludo(usuario))
+}
 
-saludar (usermaria)
-saludar (userpepe)
 
+//PILAS este silo quitas es para revisarlo en el navegador
+function saludarHtml(user) {                  
+    document.write(crearsaludo(user) + `<br>`)
+
+
+}
+
+saludar(usermaria)
+saludar(userpepe)
+saludarHtml(usermaria)
+saludarHtml(userpepe)
+
+
+if (1){}
+else {}
+
+if(1){}
+else if (2) {}
+else if (3) {}
+else {}
 
 
 
