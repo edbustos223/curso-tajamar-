99649c8999b95ebb7e3d54b5e82f28ea6dde502f
edbustos <edissn4400@hotmail.com>
2018-03-17 20:19:05 +0100

    casa js

---
+++ trabajo curso/JS CASA/02 variables/variables.js
@@ -0,0 +1,15 @@
+
+
+/*
+var nombre = prompt(`Ingresa tu nombre`);
+var edad = prompt( `ingresa tu edad`);
+
+console.log(`su nombre es ` + nombre +``+` y su edad es`+ edad)
+document.write(`su nombre es ` + nombre +``+` y su edad es`+ edad )
+alert(`su nombre es ` + nombre +``+` y su edad es`+ edad)  */
+
+
+var direccion =(`alcorcon`);
+var edadjuan =( 24 + 1)
+
+document.write(direccion + ` `+ edadjuan)
