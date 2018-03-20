94222a3bceccb32f5eb2945188a2df27cf55f781
curso-tajamar- <edissn4400@hotmail.com>
2018-03-20 21:26:43 +0100

    nuevos cambios

---
+++ trabajo curso/JS/05_APIS/arrays.js
@@ -0,0 +1,41 @@
+//aDatos es una INSANCIA de array
+let aDatos = [1, 12, 67]
+
+let aDatos2 = new Array(5)
+console.log (aDatos2.length)
+console.log (aDatos2)
+
+let aDatos3 = new Array(5,23,21,5)
+console.log(aDatos3)
+
+
+//new object
+let oDatos = {}
+
+let dni
+let code =  dni%23
+let aLetras = [ `z`,`b`,`c`,`d`,`e`,`f`,`h`,`k`,`g`,`l`,`h`,`m`,`i`,`s`,`j`,]; aLetras[code]
+
+console.log(aLetras.length)
+
+//metodos de Arrays
+//inmutables
+//mutables
+
+/*console.log(aLetras.join (` `))
+console.log(aLetras)
+console.log(aLetras.sort())
+console.log(aLetras) */
+/*aLetrasOriginal= aLetras.slice(0, aLetras.length)
+aLetras.sort()
+console.log(aLetrasOriginal) */
+
+
+//MUTABLE
+let aNuevo = aLetras.splice(0, 9, `x`,`z`)
+console.log(aNuevo)
+console.log(aLetras)
+
+//Mutables
+aLetras.push
+
---
+++ trabajo curso/JS/05_APIS/math.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+//x.algo es una INSATANCIA de string
+let x = `pepe`
+x = x.toUpperCase()
+
+
+//aDatos es una INSANCIA de array
+let aDatos = [1, 12, 67]
+
+//math NO SE INSTANCIA
+parseInt(34.09)
+Math.random(34.09)
+parseInt( Math.random()*10)
