fc1cce36e900870326a50a739321daaadab9d5dc
curso-tajamar- <edissn4400@hotmail.com>
2018-03-21 20:36:40 +0100

    c3

--- trabajo curso/JS/04FUNCIONES_AVANZADAS/JS/app3.js
+++ trabajo curso/JS/04FUNCIONES_AVANZADAS/JS/app3.js
@@ -1,43 +1,46 @@
 
-
-function calcularCuadrado (a) {
-    a*=a
+function calcularCuadrado ( a ) {
+    a *= a
     return a 
 }
 
-
-function calcularCubo (a) {
-    a= a*a*a
+function calcularCubo ( a ) {
+    a = a * a * a
     return a 
 }
+
 function MostrarCalculo(n, f) {
-    console.log(`el res`)
+    console.log(`El resultado es ${f(n)}`)    
 }
 
-let n=5
-
+/* 
 MostrarCalculo(n, calcularCuadrado)
-
 MostrarCalculo(n, calcularCubo)
-MostrarCalculo(n, function(){
-    return
-})
+MostrarCalculo(n, function() {
+    return n*2
+}) */
 
+let n = 5
 
-setTimeout(function() {
+function mostrarResultado1 () {
     console.log(calcularCubo(n))
-}, 7000)
-
-for (let i = 0; i < 1000; i++) {
-    console.log (i);
-    
 }
 
+let mostrarResultado2 = function () {
+    console.log(calcularCubo(n))
+} 
 
+setTimeout( mostrarResultado1 , 1000)
+setTimeout( mostrarResultado2 , 2000)
+setTimeout( function () {
+    console.log(calcularCubo(n))}  , 3000)
 
-    let calcularCuadrado =  function (x){
-      x()  
-    }
+for (let i = 0; i < 10000; i++) {
+    console.log(i); 
+}
 
- 
- 
+console.log(typeof mostrarResultado1)
+console.log(typeof mostrarResultado2)
+
+let miFuncion = mostrarResultado2
+console.log(typeof miFuncion)
--- trabajo curso/JS/04FUNCIONES_AVANZADAS/JS/main.js
+++ trabajo curso/JS/04FUNCIONES_AVANZADAS/JS/main.js
@@ -1,16 +1,25 @@
-function calcularCubo (a) {
-    a= a*a*a
-    return a 
-    console.log(calcularCubo(n))
+function main () {
+
+    function calcularCubo(a) {
+        a = a * a * a
+        return a 
+    }
+
+    let n = 6
+    let mostrarResultado2 = function () {
+        console.log(calcularCubo(n))
+    } 
+    console.log(typeof n)
+    console.log(typeof mostrarResultado2)
+
+    mostrarResultado2()
+
+    return calcularCubo
 }
 
-setTimeout (MostrarResultado1, 1000)
-
-
-
-
-let f = main ()
+let f = main()
 console.log(typeof f)
 console.log(f(4))
+console.log(main()(4))
 
-
+calcularCubo(5)
