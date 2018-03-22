a2a3736d3895a6f40fc716299e74ae36e4674bbd
edbustos <edissn4400@hotmail.com>
2018-03-22 14:08:39 +0100

    ej casa

--- trabajo curso/JS CASA/06_ciclo/ciclo.js
+++ trabajo curso/JS CASA/06_ciclo/ciclo.js
@@ -1,9 +1,28 @@
 
-
+/* CICLO FOR
 //var numeroDeUsuarios = 50;
 var dias = [`lunes`,`martes`,`miercoles`,`jueves`,`viernes`,`sabado`,`domingo`]
 
 for( i = 0;  i <= dias.length ; i++){
     document.write(dias [i] + `<br>` )
 }
+*/
 
+
+/*//CICLO WHILE
+var dias = [`lunes`,`martes`,`miercoles`,`jueves`,`viernes`,`sabado`,`domingo`]
+var numeroDias = dias.length -1;
+var i = 0
+while (i <= numeroDias){
+    document.write(dias[i] + `<br>`);
+    i++
+} */
+
+
+var ideaspornegocios = [`marketing`,`pagweb`,`videos corporativos`,`inbound marketing`];
+a = ideaspornegocios 
+
+for (let i = 0; i <= a.length -1; i++) {
+    document.write(a[i] + `<br>`);
+    
+}
---
+++ trabajo curso/JS CASA/07_funciones/funciones.js
@@ -0,0 +1,36 @@
+
+
+
+
+// var sumar = function(numero1, numero2){
+//     return numero1 *numero2;
+// }
+
+// document.write(sumar(200,20));
+// document.write(`<br>`);
+// document.write(sumar(500,20))
+// document.write(`<br>`);
+// document.write(sumar(600,20))
+// document.write(`<br>`);
+// document.write(sumar(100,20))
+// document.write(`<br>`);
+// document.write(sumar(700,20))
+// document.write(`<br>`);
+// document.write(sumar(900,20))
+
+
+// var numeroMaximo = function (valor1, valor2){
+//     if(valor1 > valor2){
+//         return valor1;
+//     } else {
+//         return valor2;
+//     }
+    
+// }
+
+// document.write(`el numero maximo es ` +numeroMaximo(10,20))
+// document.write(`<br>`)
+// document.write(`el numero maximo es ` +numeroMaximo(100,20))
+
+
+//--------------- CALCULADORA--
