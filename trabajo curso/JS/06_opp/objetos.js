06abe9a6c4f75b69d1b626f06497814e08bc84c9
curso-tajamar- <edissn4400@hotmail.com>
2018-03-22 18:30:49 +0100

    factura

--- trabajo curso/JS/06_opp/objetos.js
+++ trabajo curso/JS/06_opp/objetos.js
@@ -1,37 +1,46 @@
- //let oDatos = new Object = {}
-
-    let libro1 = {
-        autor: 'J. R. R. Tolkien',
-        titulo: 'El Señor de los Anillos',
-        editorial: 'Minotauro',
-        fecha: new Date(1954),
-        generos: ['Fantasia'],
-        mostrarReferencia: function () {
-            let ref = this.autor + `. `
-            ref += `"` + this.titulo + `"` + `. `
-            ref += this.editorial + `. ` + this.fecha + `. `
-    
-          onsole.log(ref)
-    
-        }
+let libro1 = {
+    autor: 'J. R. R. Tolkien',
+    titulo: 'El Señor de los Anillos',
+    editorial: 'Minotauro',
+    fecha: new Date('1954'),
+    generos: ['Fantasia'],
+    mostrarReferencia: function () {
+        let ref = this.autor + '. '
+        ref += '"' + this.titulo + '". '
+        ref += this.editorial + ', ' + this.fecha.getFullYear() + '. '
+        console.log(ref)
     }
-    
-    //libro1.mostrarReferencia()
-    
-    let miFunction = function(){
-        console.log(`soy una funcion`)
-        console.log(this)
-    }
-    //miFunction()
-
-    let o = {head: `soy un objeto`,
-            metodo:miFunction        
 }
-//o.metodo()
 
-let oNuevoObjeto = new miFunction()
-                                    
+libro1.mostrarReferencia()
 
-let obj = {head: `soy un objeto`}   
-miFunction.call(obj) 
-    
+let miFuncion = function() {
+    console.log ('Soy una funcion')
+    console.log(this)
+}
+
+/* Opciones de invocación
+La diferencia entre funciones y métodos está en el patrón de invocación , 
+que determina cual es el valor de this */
+
+// Patrón de invocación Function
+// this es el objeto global del programa
+miFuncion()
+
+let o = {head: 'Soy un objeto',
+        metodo: miFuncion }
+// Patrón de invocación Method
+// this es el objeto en el que esta el método
+o.metodo()
+
+// Patrón de invocación Constructor
+// this es el nuevo objeto creado
+
+let MiFuncion = miFuncion
+let oNuevoObjeto = new MiFuncion()
+
+// Patrón de invocación Apply
+// permite definir el valor de this
+
+let obj = {head: 'Soy un objeto'}
+miFuncion.call(obj)
---
+++ trabajo curso/JS/06_opp/params.js
@@ -0,0 +1,20 @@
+
+
+function Point (param){
+    this.x= param.x || 0
+    this.y= param.y || 0
+    this.r= param.r || 1
+    this.calcularCircunferencia = function (){}
+    this.calcularCirculo = function (){}
+    this.calcularOtro = function (){}
+    this.calcularOtro1 = function (){}
+
+}
+
+p1 = new Point ({x:2, y:3, r:5})
+p1.calcularOtro
+p1.calcularCirculo
+
+
+function App(param)
+app = new app({user: `pepe`})
